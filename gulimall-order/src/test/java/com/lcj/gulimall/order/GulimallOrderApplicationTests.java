c8cb4269644d6237aa36fcd3e62390a20b348c81
LCJ <1982433960@qq.com>
2023-06-28 17:40:04 +0800

    项目初试结构创建

---
+++ gulimall-order/src/test/java/com/lcj/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcj.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
